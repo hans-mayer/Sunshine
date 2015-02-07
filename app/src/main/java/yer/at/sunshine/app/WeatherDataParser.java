971ab449a48e30970baa2ace94fcfb55191d463a
hans-mayer <github@ma.yer.at>
2015-02-07 23:37:58 +0100

    ende lesson 2

---
+++ app/src/main/java/yer/at/sunshine/app/WeatherDataParser.java
@@ -0,0 +1,21 @@
+package yer.at.sunshine.app;
+
+import org.json.JSONException;
+
+/**
+ * Created by mayer on 2/7/2015.
+ */
+public class WeatherDataParser {
+    /**
+     * Given a string of the form returned by the api call:
+     * http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7
+     * retrieve the maximum temperature for the day indicated by dayIndex
+     * (Note: 0-indexed, so 0 would refer to the first day).
+     */
+    public static double getMaxTemperatureForDay(String weatherJsonStr, int dayIndex)
+            throws JSONException {
+        // TODO: add parsing code here
+        return -1;
+    }
+
+}
